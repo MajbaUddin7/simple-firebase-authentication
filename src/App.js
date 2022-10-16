be195733ffa5bffbbc5b9c69fc63cfbff14d9342
majbauddi7 <majbauddinsabbir7@gmail.com>
2022-10-17 01:07:40 +0600

    Using tuggal in singIn & SingOut button

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-
 import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
-
-const auth = getAuth(app);
+const auth = getAuth();
 
 function App() {
 
+  const [user, setUser] = useState({})
   const provider = new GoogleAuthProvider();
 
-  const handleGoogleSignIn = () => {
+  const handleGoogleSingIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user);
       })
       .catch(error => {
         console.error('error: ', error);
+      })
+  }
 
+  const handleSingOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
       })
   }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sing In</button>
+      {/* condition ? true: false */}
 
+      {user.email ?
+        <button onClick={handleSingOut}>Sing Out</button>
+        :
+        <button onClick={handleGoogleSingIn}>Google Sing In</button>
+      }
+      {user.email && <div className="">
+        <h3>User Name: {user.displayName}</h3>
+        <p>Email address: {user.email}</p>
+        <img src={user.photoURL} alt="" />
+      </div>}
     </div>
   );
 }
